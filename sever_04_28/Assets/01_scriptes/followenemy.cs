e90aa0865c107982e73ccb0bda0f57a808091ae6
damin06 <98873820+damin06@users.noreply.github.com>
2022-06-10 23:15:42 +0900

    map
    맵

--- sever_04_28/Assets/01_scriptes/followenemy.cs
+++ sever_04_28/Assets/01_scriptes/followenemy.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class followenemy : MonoBehaviour
 {
+    Animator animator;
     [SerializeField]
     Transform player;
 
@@ -14,45 +16,53 @@ public class followenemy : MonoBehaviour
     float movespeed;
     int rnadomwait; 
     Rigidbody2D rb2d;
+    private float curtime;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
        rnadomwait= Random.Range(1,3);
+       animator.SetTrigger("spawn");
     }
 
     // Update is called once per frame
     void Update()
     {
+        curtime+=Time.deltaTime;
+        
         float distoplayer = Vector2.Distance(transform.position,player.position);
-        if(distoplayer<agroRange)
+        if(distoplayer<agroRange && curtime>1)
         {
-          ChasePlayer();
-          
+          StartCoroutine(ChasePlayer());
+          StopCoroutine(SotpChase());
         }
         else
         {
-         SotpChase();
-        }
+        StopCoroutine(ChasePlayer());
+         StartCoroutine(SotpChase());
+        }     
     }
-    void ChasePlayer()
+    IEnumerator ChasePlayer()
     {
      if(transform.position.x<player.position.x)
      {
-      rb2d.velocity = new Vector2(movespeed,0);  
+      rb2d.velocity = new Vector3(movespeed,0);  
        //transform.eulerAngles= new Vector3(0,0,0);
-      transform.eulerAngles = new Vector2(1,1);       
+      transform.eulerAngles = new Vector3(0,180,0);       
      }
      else
      {
           //transform.eulerAngles= new Vector3(0,180,0);
-          rb2d.velocity = new Vector2(-movespeed,0);
-          transform.eulerAngles = new Vector2(-1,1);    
+          rb2d.velocity = new Vector3(-movespeed,0);
+          transform.eulerAngles = new Vector3(0,0,0);    
      }
+     yield return null;
     }
-    void SotpChase()
+    IEnumerator SotpChase()
     {
-       rb2d.velocity = new Vector2(0,0);
+       rb2d.velocity = new Vector3(0,0,0);
+       yield return null;
     }
     IEnumerator randomfollow()
     { if(transform.position.x<player.position.x)
